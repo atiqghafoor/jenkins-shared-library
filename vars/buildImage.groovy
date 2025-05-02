b0c43268d642bb7c05b55ddf9cd655d0cd637b73
Atiq Ur Rehman <atiq_36@yahoo.com>
2025-05-02 23:14:20 +0500

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -2,9 +2,9 @@
 
 def call() {
     echo "building the docker image..."
-    withCredentials([usernamePassword(credentialsId: 'docker-hub-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
-        sh 'docker build -t nanatwn/demo-app:jma-2.0 .'
-        sh 'echo $PASS | docker login -u $USER --password-stdin'
-        sh 'docker push nanatwn/demo-app:jma-2.0'
+    withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){    
+        sh 'docker build -t 137.202.47.31:8083/demo-app:jma-2.0 .'
+        sh 'echo $PASS | docker login -u $USER --password-stdin 137.202.47.31:8083'
+        sh 'docker push 137.202.47.31:8083/demo-app:jma-2.0'
     }
 }
