f155e18559d7bf5c49aa93e7b961932be1b3bec3
Atiq Ur Rehman <atiq_36@yahoo.com>
2025-05-03 21:54:01 +0500

    Update buildImage.groovy

--- vars/buildImage.groovy
+++ vars/buildImage.groovy
@@ -1,10 +1,10 @@
 #!/user/bin/env groovy
 
-def call() {
+def call(String imageNmae) {
     echo "building the docker image..."
     withCredentials([usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]){    
-        sh 'docker build -t 137.202.47.31:8083/demo-app:jma-3.0 .'
+        sh "docker build -t $imageNmae ."
         sh 'echo $PASS | docker login -u $USER --password-stdin 137.202.47.31:8083'
-        sh 'docker push 137.202.47.31:8083/demo-app:jma-3.0'
+        sh "docker push $imageNmae"
     }
 }
