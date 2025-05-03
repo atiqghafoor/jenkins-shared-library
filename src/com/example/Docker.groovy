ed18441aa8bd79746bf75345c1fe18ffa9bf9e1d
Atiq Ur Rehman <atiq_36@yahoo.com>
2025-05-03 22:46:42 +0500

    Update and rename Docker_old_1.groovy to Docker.groovy

---
+++ src/com/example/Docker.groovy
@@ -0,0 +1,20 @@
+#!/user/bin/env groovy
+package com.example
+
+class Docker implements Serializable {
+
+    def script
+
+    Docker(script) {
+        this.script = script
+    }
+
+    def buildDockerImage(String imageName) {
+        script.echo "building the docker image..."
+        script.sh "docker build -t $imageName ."
+        script.withCredentials([script.usernamePassword(credentialsId: 'nexus-docker-repo', passwordVariable: 'PASS', usernameVariable: 'USER')]) {
+            script.sh "echo '${script.PASS}' | docker login -u '${script.USER}' --password-stdin 137.202.47.31:8083"
+            script.sh "docker push $imageNmae"
+        }
+    }
+}
